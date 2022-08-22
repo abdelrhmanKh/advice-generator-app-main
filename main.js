3a281ded5a2c6683a9e41da5c806b9fc88a58aba
Abdelrhman khalil Ali <81552316+abdelrhmanKh@users.noreply.github.com>
2022-08-23 01:24:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const btn = document.querySelector(".get-advice");
 
 function getAdvice() {
 
-    fetch(`https://api.adviceslip.com/advice/${Math.floor(Math.random() * 224)}`)
+    fetch(`https://api.adviceslip.com/advice/${Math.floor(Math.random() * 200)}`)
         .then((response) => response.json())
         .then((data) => {
             advice.innerHTML = `“${data.slip.advice}”`;
